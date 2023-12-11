386032b1442e669776f1700af1f3fd8a0b84459d
Benamirouche Abdelhak <abdelhak.benamirouche@g.enp.edu.dz>
2023-12-11 14:12:09 +0100

    Update heatmap.py

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -1,69 +1,27 @@
-import dlib
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import face_recognition
 
-def detect_landmarks(image):
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Initialize the dlib face detector and facial landmarks predictor
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-    
+def heatmap_generator(image):
+    face_locations = face_recognition.face_locations(image)
 
-    # Detect faces in the image
-    faces = detector(gray)
-    
-    # Get the facial landmarks
-    for face in faces:
-        landmarks = predictor(gray, face)
-    
-    # Draw the facial landmarks
-    for i in range(68):
-        x = landmarks.part(i).x
-        y = landmarks.part(i).y
-        cv2.circle(image, (x, y), 2, (255, 255, 255), -1)
-    
-    return image, landmarks
+    # Load the pre-trained facial landmark model
+    face_landmarks_list = face_recognition.face_landmarks(image, face_locations)
 
-def heatmap_generation(image,landmarks):
-    
-    # The image dimensions
+    # Get image dimensions
     height, width = image.shape[:2]
-    
-    # Create a blank heatmap with the same dimensions as the original image
-    heatmap = np.zeros([height,width])
-    print(heatmap.shape)
-    # Draw the landmarks on the heatmap
-    for i in range(0, 68):
-        x = landmarks.part(i).x
-        y = landmarks.part(i).y
-        # Set pixel value to 1 at landmark position
-        heatmap[y, x] = 1
 
-    # Apply gaussian filter
-    filtered_heatmap = dlib.gaussian_blur(heatmap,3)[0]
+    # Generate an empty numpy array with the image dimensions
+    lm = np.zeros([height,width])
 
-    return filtered_heatmap
-    
-# Testing the functions
+    # Draw facial landmarks on the image
+    for face_landmarks in face_landmarks_list:
+        for landmark_type, landmarks in face_landmarks.items():
+            for (x, y) in landmarks:
+                if x < height and y < width:
+                    lm[y,x] = 1
 
-# # The image path
-# image_path = "./example.jpg"
+    heatmap = cv2.GaussianBlur(lm, [59,59], 3)         
 
-# # Read the image
-# image = cv2.imread(image_path)
-# # Landmarks detection
-# result, landmarks = detect_landmarks(image)
-
-# # Display the image with landmarks
-# # cv2.imshow("Facial Landmarks", result)
-# # cv2.waitKey(0)
-# # cv2.destroyAllWindows()
-
-# # Heatmap generation
-# heatmap = heatmap_generation(image, landmarks)
-
-# # Display the heatmap
-# plt.imshow(heatmap, cmap=plt.cm.gray)
+    return heatmap
