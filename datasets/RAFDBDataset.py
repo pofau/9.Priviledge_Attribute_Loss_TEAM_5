d6c2469af9cae05f2769fde39d2c97ec210de2ab
Matisc8 <118530291+Matisc8@users.noreply.github.com>
2023-12-11 13:21:27 +0000

    new file:   datasets/RAF-DB/Image/aligned/labels/test_label.txt 	renamed:    datasets/RAF-DB/Image/aligned/label.txt -> datasets/RAF-DB/Image/aligned/labels/train_label.txt 	modified:   datasets/RAFDBDataset.py 	modified:   ik/vgg16_train.ipynb

--- datasets/RAFDBDataset.py
+++ datasets/RAFDBDataset.py
@@ -1,29 +1,31 @@
 import os
-from PIL import Image
+import torch
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-
-import os
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
 
 class RAFDBDataset(Dataset):
-    def __init__(self, root_dir, label_dir, transform=None):
+    def __init__(self, root_dir, label_dir, subset, label_file_name, transform=None):
         self.root_dir = root_dir
         self.label_dir = label_dir
         self.transform = transform
+        self.subset = subset
+        self.label_file_name = label_file_name
         self.labels, self.image_paths = self._load_data()
 
     def _load_data(self):
         labels = []
         image_paths = []
-
-        labels_file_path = os.path.join(self.label_dir, 'list_partition_label.txt')
+        
+        labels_file_path = os.path.join(self.label_dir, self.label_file_name)
         with open(labels_file_path, 'r') as file:
             lines = file.readlines()
 
             for line in lines:
                 parts = line.strip().split(' ')
                 label = int(parts[1])
-                image_path = os.path.join(self.root_dir, 'aligned', parts[0])  # Change 'aligned' to the desired folder
+                image_path = os.path.join(self.root_dir, self.subset, parts[0])
                 labels.append(label)
                 image_paths.append(image_path)
 
@@ -35,7 +37,6 @@ class RAFDBDataset(Dataset):
     def __getitem__(self, idx):
         label = self.labels[idx]
         image_path = self.image_paths[idx]
-        
         image = Image.open(image_path).convert('RGB')
 
         if self.transform:
@@ -43,10 +44,7 @@ class RAFDBDataset(Dataset):
 
         return image, label
 
-    
 
-
-# Transform function for training data
 train_transform = transforms.Compose([
     transforms.RandomRotation(degrees=(-10, 10)),
     transforms.RandomHorizontalFlip(),
@@ -55,21 +53,44 @@ train_transform = transforms.Compose([
 ])
 
 # Transform function for test data
-test_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+transform = transforms.Compose([
+
     transforms.ToTensor(),
 ])
 
 root_dir = '../datasets/RAF-DB/Image/aligned/'
-label_dir = '../datasets/RAF-DB/Image/aligned/'
+label_dir = '../datasets/RAF-DB/Image/aligned/labels'
 
 
 
 # Assuming you have already defined full_dataset, train_subset, and test_subset
-train_dataset = RAFDBDataset(root_dir=root_dir, label_dir = label_dir)
-test_dataset = RAFDBDataset(root_dir=root_dir, label_dir = label_dir)
+train_dataset = RAFDBDataset(root_dir=root_dir, label_dir = label_dir, subset = 'train', label_file_name='train_label.txt', transform=transform)
+test_dataset = RAFDBDataset(root_dir=root_dir, label_dir = label_dir, subset = 'test', label_file_name='test_label.txt', transform=transform)
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+# Charger le modèle pré-entraîné VGG16
+base_model = torchvision.models.vgg16(pretrained=True)
+# Supprimer la dernière couche entièrement connectée
+base_model.classifier = nn.Sequential(*list(base_model.classifier.children())[:-1])
+
+# Ajouter une nouvelle couche adaptée à 7 classes
+num_classes = 7
+classifier_layer = nn.Linear(4096, num_classes)
+model = nn.Sequential(base_model, classifier_layer)
+
+# Afficher la structure du modèle
+summary(model, (3, 224, 224))  # Assurez-vous d'ajuster les dimensions en fonction de vos données
+
+# Identifier la dernière couche de convolution
+last_conv_layer = model[0].features[28]
+print(last_conv_layer)
+optimizer = optim.Adam(model.parameters(), lr=4e-5)
+
+# Fonction pour enregistrer le gradient
+def save_gradient(grad):
+    global conv_output_gradient
+    conv_output_gradient = grad
 
 print("RAF-DB Dataset Loaded !")
