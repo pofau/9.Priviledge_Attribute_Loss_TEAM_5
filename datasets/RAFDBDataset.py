c2e29c9c158bef753a68ba93cb405ea13e6673c9
Matisc8 <118530291+Matisc8@users.noreply.github.com>
2023-12-11 11:56:57 +0000

    modified:   datasets/RAFDBDataset.py

--- datasets/RAFDBDataset.py
+++ datasets/RAFDBDataset.py
@@ -1,46 +1,47 @@
 import os
-import torch
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
 from PIL import Image
-import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+import os
 
 class RAFDBDataset(Dataset):
-    def __init__(self, root_dir, label_path, subset, transform=None):
+    def __init__(self, root_dir, label_dir, transform=None):
         self.root_dir = root_dir
-        self.label_path = label_path
+        self.label_dir = label_dir
         self.transform = transform
-        self.classes = sorted([f for f in os.listdir(os.path.join(root_dir, subset)) if os.path.isdir(os.path.join(root_dir, subset, f))])
-        self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
-        self.samples = []
+        self.labels, self.image_paths = self._load_data()
 
-        with open(label_path, 'r') as file:
+    def _load_data(self):
+        labels = []
+        image_paths = []
+
+        labels_file_path = os.path.join(self.label_dir, 'list_partition_label.txt')
+        with open(labels_file_path, 'r') as file:
             lines = file.readlines()
 
             for line in lines:
                 parts = line.strip().split(' ')
-                file_name = parts[0]
-                cls_name = parts[1]
-                cls_idx = self.class_to_idx[cls_name]
+                label = int(parts[1])
+                image_path = os.path.join(self.root_dir, 'aligned', parts[0])  # Change 'aligned' to the desired folder
+                labels.append(label)
+                image_paths.append(image_path)
 
-                image_path = os.path.join(root_dir, subset, file_name)
-                self.samples.append((image_path, cls_idx))
+        return labels, image_paths
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        file_path, cls_idx = self.samples[idx]
-
-        image = Image.open(file_path)
+        label = self.labels[idx]
+        image_path = self.image_paths[idx]
+        
+        image = Image.open(image_path).convert('RGB')
 
         if self.transform:
             image = self.transform(image)
 
-        return image, cls_idx
+        return image, label
 
     
 
@@ -59,10 +60,14 @@ test_transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+root_dir = '../datasets/RAF-DB/Image/aligned/'
+label_dir = '../datasets/RAF-DB/Image/aligned/'
+
+
 
 # Assuming you have already defined full_dataset, train_subset, and test_subset
-train_dataset = RAFDBDataset(root_dir="C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned", label_dir = r'C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned\list_partition_label.txt', transform=train_transform)
-test_dataset = RAFDBDataset(root_dir=r'C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned', label_dir = r'C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned\list_partition_label.txt',transform=test_transform)
+train_dataset = RAFDBDataset(root_dir="C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned\train", label_dir = r'C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned', transform=train_transform)
+test_dataset = RAFDBDataset(root_dir=r'C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned\test', label_dir = r'C:\Users\MCE30\Desktop\SAR\M2 SAR\MLA\Projet\RAF-DB\Image\aligned\list_partition_label.txt',transform=test_transform)
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
