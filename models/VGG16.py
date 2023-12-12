8c68c9119d2ce9bd6d42e0e1d6587d8c41c15883
pofau <98581288+pofau@users.noreply.github.com>
2023-12-12 09:07:46 +0100

    Update VGG16

--- models/VGG16.py
+++ models/VGG16.py
@@ -1,78 +1,47 @@
-import torchvision.models as models
 import torch
+from torch.utils.data import DataLoader, random_split, Subset
+from torchvision import transforms, models
 import torch.nn as nn
+from torchsummary import summary
+import torch.optim as optim
+from datasets import load_dataset  # Assurez-vous que la bibliothèque `datasets` est installée
 
-# modèle VGG16 pré-entraîné
-vgg16_model = models.vgg16(pretrained=True)
+class AffectNetClassifier(nn.Module):
+    def __init__(self, num_classes=7, train_size_ratio=0.2, batch_size=16, learning_rate=4e-5):
+        super(AffectNetClassifier, self).__init__()
+        # Charger le dataset
+        full_dataset = load_dataset("Piro17/affectnethq", split='train')
+        train_size = int(train_size_ratio * len(full_dataset))
+        test_size = len(full_dataset) - train_size
+        train_subset, test_subset = random_split(full_dataset, [train_size, test_size])
 
-# Adapte la dernière couche FC pour le nombre de classes dans AffectNet
-num_classes = 6  # AffectNet a 6 classes d'émotions
-vgg16_model.classifier[6] = nn.Linear(vgg16_model.classifier[6].in_features, num_classes)
+        # Définir les transformations
+        self.train_transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomRotation((-10, 10)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+        self.test_transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+        ])
 
+        # Créer les datasets et les dataloaders
+        self.train_loader = DataLoader(AffectNetHqDataset(Subset(full_dataset, train_subset.indices), transform=self.train_transform), 
+                                       batch_size=batch_size, shuffle=True)
+        self.test_loader = DataLoader(AffectNetHqDataset(Subset(full_dataset, test_subset.indices), transform=self.test_transform), 
+                                      batch_size=batch_size, shuffle=False)
 
-class VGG16(nn.Module):
-    def __init__(self):
-        super(VGG16, self).__init__()
-
-        self.conv_layers = nn.Sequential(
-            # Premier bloc
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Deuxième bloc
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Troisième bloc
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Quatrième bloc
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Cinquième bloc
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.fc_layers = nn.Sequential(
-            # Couches entièrement connectées
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, 1000),
-            nn.Softmax(dim=1)
-        )
+        # Configuration du modèle VGG16
+        self.model = models.vgg16(pretrained=True)
+        self.model.classifier = nn.Sequential(*list(self.model.classifier.children())[:-1])
+        self.model.classifier.add_module('final_fc', nn.Linear(4096, num_classes))
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
     def forward(self, x):
-        x = self.conv_layers(x)
-        x = self.fc_layers(x)
-        return x
+        return self.model(x)
 
-# Création du modèle VGG16
-vgg16 = VGG16()
+# Utilisation
+classifier = AffectNetClassifier()
+summary(classifier.model, (3, 224, 224))
