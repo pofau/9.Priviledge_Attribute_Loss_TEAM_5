0e8b0bda725f10a72f504d492d29047140818424
pofau <98581288+pofau@users.noreply.github.com>
2023-12-17 23:47:18 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .pal import PAL
-from .resnet50 import ResNet50
+from .pal import PrivilegedAttributionLoss
+from .resnet50 import CustomResNet
 from .VGG16 import VGG16
 from .VGGFace import VGGFace
