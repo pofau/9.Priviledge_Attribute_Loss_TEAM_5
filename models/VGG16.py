4f4c3a8a308961c58794d33cb8859867c225d23b
pofau <98581288+pofau@users.noreply.github.com>
2023-12-17 23:33:16 +0100

    Update VGG16.py

--- models/VGG16.py
+++ models/VGG16.py
@@ -4,18 +4,18 @@ from torchvision import transforms, models
 import torch.nn as nn
 from torchsummary import summary
 import torch.optim as optim
-from datasets import load_dataset  # Assurez-vous que la bibliothèque `datasets` est installée
+from datasets import load_dataset  # Make sure the `datasets` library is installed
 
 class AffectNetClassifier(nn.Module):
     def __init__(self, num_classes=7, train_size_ratio=0.2, batch_size=16, learning_rate=4e-5):
         super(AffectNetClassifier, self).__init__()
-        # Charger le dataset
+        # Load the dataset
         full_dataset = load_dataset("Piro17/affectnethq", split='train')
         train_size = int(train_size_ratio * len(full_dataset))
         test_size = len(full_dataset) - train_size
         train_subset, test_subset = random_split(full_dataset, [train_size, test_size])
 
-        # Définir les transformations
+        # Define transformations
         self.train_transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.RandomRotation((-10, 10)),
@@ -27,13 +27,13 @@ class AffectNetClassifier(nn.Module):
             transforms.ToTensor(),
         ])
 
-        # Créer les datasets et les dataloaders
+        # Create datasets and dataloaders
         self.train_loader = DataLoader(AffectNetHqDataset(Subset(full_dataset, train_subset.indices), transform=self.train_transform), 
                                        batch_size=batch_size, shuffle=True)
         self.test_loader = DataLoader(AffectNetHqDataset(Subset(full_dataset, test_subset.indices), transform=self.test_transform), 
                                       batch_size=batch_size, shuffle=False)
 
-        # Configuration du modèle VGG16
+        # Configure the VGG16 model
         self.model = models.vgg16(pretrained=True)
         self.model.classifier = nn.Sequential(*list(self.model.classifier.children())[:-1])
         self.model.classifier.add_module('final_fc', nn.Linear(4096, num_classes))
@@ -42,6 +42,6 @@ class AffectNetClassifier(nn.Module):
     def forward(self, x):
         return self.model(x)
 
-# Utilisation
-classifier = AffectNetClassifier()
-summary(classifier.model, (3, 224, 224))
+if __name__ == '__main__':
+    classifier = AffectNetClassifier()
+    summary(classifier.model, (3, 224, 224))
